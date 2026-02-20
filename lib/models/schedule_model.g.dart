06818ad8664abc1f546e7bc04bbd646135c15615
zero_signal <zerosignal593@gmail.com>
2026-02-20 05:30:06 -0500

    Add generated Hive adapter file

---
+++ lib/models/schedule_model.g.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'schedule_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ScheduleAdapter extends TypeAdapter<Schedule> {
+  @override
+  final int typeId = 0;
+
+  @override
+  Schedule read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return Schedule(
+      title: fields[0] as String,
+      description: fields[1] as String,
+      time: fields[2] as DateTime,
+      isCompleted: fields[3] as bool,
+      type: fields[4] as String,
+      isRecurring: fields[5] as bool,
+      recurringDays: (fields[6] as List).cast<int>(),
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, Schedule obj) {
+    writer
+      ..writeByte(7)
+      ..writeByte(0)
+      ..write(obj.title)
+      ..writeByte(1)
+      ..write(obj.description)
+      ..writeByte(2)
+      ..write(obj.time)
+      ..writeByte(3)
+      ..write(obj.isCompleted)
+      ..writeByte(4)
+      ..write(obj.type)
+      ..writeByte(5)
+      ..write(obj.isRecurring)
+      ..writeByte(6)
+      ..write(obj.recurringDays);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ScheduleAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
